24dca8f795b10eeaac464d1f78333adbc6bea85a
Visnja <visnjax@gmail.com>
2019-11-12 22:05:58 +0100

    khdfahfdljfhkafds

---
+++ WraperKlase/src/ugradjene/klase/GlavnaKlasa.java
@@ -0,0 +1,98 @@
+package ugradjene.klase;
+
+public class GlavnaKlasa {
+
+	public static void main(String[] args) {
+		
+		
+/*		char karakter = 'u';
+		
+		Enkapsulacija.daLiJeSlovo(karakter);
+		
+		
+		
+		
+		String x = "2";
+		String y = "5";
+		
+		
+		int prviBroj = Integer.parseInt(x);
+		int drugiBroj = Integer.parseInt(y);
+		
+	
+		System.out.println(x+y);
+		System.out.println(prviBroj + drugiBroj);
+		*/
+		
+		
+		
+		String a = "pera";
+		String b = "detlic";
+		
+		System.out.println(a+b);
+		
+		
+		
+		StringBuilder builder = new StringBuilder ();
+		builder.append(a);
+		builder.append(b);
+		
+		System.out.println(builder);
+		
+		// duzina Stringa
+		
+		String rec = "otorinolaringologija";
+		int brojSlova = rec.length ();
+		
+		System.out.println(brojSlova);
+	
+		
+		Enkapsulacija.brojacSlova(rec);
+		
+		
+		
+		
+		
+		
+		
+		
+		// prolazak kroz String i uzimanje karaktera
+		
+		
+		String password = "Mi ucimo javu";
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
